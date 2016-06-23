8470f0c31a01aa1b9e29d374f2c68af5ae764c81
holfyuen <soundgoeson@yahoo.com.hk>
2016-06-23 09:15:53 +0800

    CNY app

--- server.R
+++ server.R
@@ -1,41 +1,21 @@
 library(shiny)
-library(quantmod)
-
-# Download data for a stock if needed, and return the data
-# From reference
-Sys.setlocale("LC_TIME", "English")
-require_symbol <- function(symbol, envir = parent.frame()) {
-  if (is.null(envir[[symbol]])) {
-    envir[[symbol]] <- getSymbols(symbol, src="oanda", auto.assign = FALSE)
-  }
-  
-  envir[[symbol]]
-}
 
 shinyServer(function(input, output) {
-  # Create an environment for storing data
-  symbol_env <- new.env()
-  
-  # Make a chart for a symbol, with the settings from the inputs
-  make_chart <- function(symbol) {
-    x<-paste(symbol,"/CNY",sep="")
-    symbol_data <- require_symbol(x, symbol_env)
-    
-    if (input$period==1) {
-    chartSeries(symbol_data,
-                name      = symbol,
-                subset    = paste(input$date, collapse = "::"),
-                theme     = chartTheme("white",up.col="blue"))
-    }
-    else {
-      chartSeries(to.weekly(symbol_data),
-                  name      = symbol,
-                  subset    = paste(input$date, collapse = "::"),
-                  theme     = "white")      
-    }
-  }
-  
-  output$chart <- renderPlot({ make_chart(input$code) })
 
+  output$chart <- renderPlot({
+    curr<-read.csv(input$code)
+    curr$Date<-as.Date(curr$Date)
+    plot(curr,type="l",col=input$color,lwd=input$thickness,ylab="Rate")
+    })
+
+  output$text<- renderText(
+{"This app intends to show the chart of Chinese Renminbi exchange rate against US Dollar, Euro, British Pound and Bitcoin from 2015-1-1 to 2016-6-17.
+User can choose line width and color in its chart.
+Exchange rate data is retrieved from Quandl with the following codes:
+US Dollar: FED/RXI_N_B_CH
+Euro: ECB/EURCNY
+British Pound: BOE/XUDLBK89
+Bitcoin: BAVERAGE/CNY
+  "})
 }
 )
--- ui.R
+++ ui.R
@@ -4,25 +4,28 @@ library(shiny)
 shinyUI(pageWithSidebar(
   
   # Application title
-  headerPanel("Renminbi Price"),
+  headerPanel("Renminbi Rate"),
   
   sidebarPanel(
     radioButtons("code", label=h3("Renminbi vs:"),
-                choices=list("US Dollar" = "USD", 
-                     "Euro" = "EUR", 
-                     "Japanese Yen" = "JPY",
-                     "Bitcoin" = "BTC")),
+                choices=list("US Dollar" = "data/usdcny.csv", 
+                     "Euro" = "data/eurcny.csv", 
+                     "British Pound" = "data/gbpcny.csv",
+                     "Bitcoin" = "data/btccny.csv")),
+    radioButtons("color", label=h3("Color"),
+                 choices=list("Black" = "black", 
+                              "Blue" = "blue", 
+                              "Green" = "green",
+                              "Red" = "red")),
+    sliderInput("thickness",label=h3("Thickness"),min=1,max=7,value=1,step=1)
     
-    dateRangeInput(inputId = "date", label = h3("Date range"),
-                   start = Sys.Date() - 180, end = Sys.Date()),
-    radioButtons("period", label=h3("Chart"),
-                 choices=list("Daily" = 1, 
-                              "Weekly" = 2 
-                              ))
   ),
   
   mainPanel(
-    plotOutput("chart")
+    tabsetPanel(
+    tabPanel("Plot",plotOutput("chart")),
+    tabPanel("About",verbatimTextOutput("text"))
+    )
     
   )
 ))
